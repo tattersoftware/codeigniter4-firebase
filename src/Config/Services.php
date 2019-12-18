41ac641a716fa744a04a0057f14405f1c36ad2e2
MGatner <mgatner@icloud.com>
2019-12-18 22:33:50 +0000

    Fix docs typo

--- src/Config/Services.php
+++ src/Config/Services.php
@@ -11,7 +11,7 @@ class Services extends BaseService
 	 * @param mixed    $serviceAccount  Anything accepted by ServiceAccount::fromValue()
 	 * @param boolean  $getShared
 	 *
-	 * @return \CodeIgniter\HTTP\CLIRequest
+	 * @return \Tatter\Firebase\Firebase
 	 */
 	public static function firebase($serviceAccount = null, bool $getShared = true): Firebase
 	{
