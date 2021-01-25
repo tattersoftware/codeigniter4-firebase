bddd866af7d56be84afe8524709b49e43d37c320
MGatner <mgatner@icloud.com>
2021-01-25 03:02:54 +0000

    Fix SA

--- src/Model.php
+++ src/Model.php
@@ -245,7 +245,7 @@ class Model
 	 */
 	public function reference(string $uid): DocumentReference
 	{
-		return $this->builder(null, true)->document($uid);
+		return $this->builder(null, true)->document($uid); // @phpstan-ignore-line
 	}
 
 	//--------------------------------------------------------------------
