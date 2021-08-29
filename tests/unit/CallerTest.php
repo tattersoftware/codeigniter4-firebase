4c1613ddb42accaa0d4cd9755682160b30338c5f
MGatner <mgatner@icloud.com>
2021-08-29 20:10:49 +0000

    Fix workflows

---
+++ tests/unit/CallerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use CodeIgniter\Test\CIUnitTestCase;
+use Config\Services;
+
+class CallerTest extends CIUnitTestCase
+{
+	public function testGetErrors()
+	{
+		$caller = service('firebase')->caller;
+
+		$this->assertSame([], $caller->getErrors());
+	}
+
+	public function testSetUid()
+	{
+		$caller = service('firebase')->caller;
+
+		$caller->setUid('banana');
+		$result = $this->getPrivateProperty($caller, 'uid');
+
+		$this->assertSame('banana', $result);
+	}
+}
